c7c9c0417ba994232d418854aede275cf702930a
Robin Nap <64479150+RobinNap@users.noreply.github.com>
2022-10-07 16:03:48 +0200

    Add files via upload

---
+++ Music_VideosUITests/Music_VideosUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Music_VideosUITestsLaunchTests.swift
+//  Music_VideosUITests
+//
+//  Created by Robin Nap on 05/10/2022.
+//
+
+import XCTest
+
+final class Music_VideosUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
